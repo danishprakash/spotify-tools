fab9a12870d03d62995ffb4880d7eeacf4aa2108
prakashdanish <grafitykoncept@gmail.com>
2017-05-27 10:12:05 +0530

    added playlist title to be used as file name

--- plist-text.py
+++ plist-text.py
@@ -1,12 +1,13 @@
-import bs4
-import requests
+import bs4, requests, re
 
-playlist = open('playlist.txt', 'w')
-
-playlistReq = requests.get('https://open.spotify.com/user/danishprakash/playlist/4MsaDGPQ8SX7k2tvaHAU36')
-playlistReq.raise_for_status()
+playlistReq =requests.get('https://open.spotify.com/user/danishprakash/playlist/4MsaDGPQ8SX7k2tvaHAU36')
 playlistSoup = bs4.BeautifulSoup(playlistReq.text, "lxml")
 songUrl = playlistSoup.find_all('meta', property='music:song')
+
+reg = re.sub(r', a playlist.*', '', playlistSoup.find('meta', property='og:title')['content'])
+
+playlist = open(str(reg+'.txt'), 'w')
+playlist.write(reg+'\n\n')
 
 for i in range(len(songUrl)):
     songReq = requests.get(songUrl[i]['content'])
@@ -17,8 +18,8 @@ for i in range(len(songUrl)):
     artistReq = requests.get(artistUrl['content'])
     artistSoup = bs4.BeautifulSoup(artistReq.text, "lxml")
     artistName = artistSoup.find('meta', property='og:title')
-    temp = (str(i+1) + '. ' + songName['content']+' - ' +
+    playlist.write(str(i+1) + '. ' + songName['content']+' - ' +
             artistName['content']+ '\n')
-    playlist.write(temp)
+
 
 playlist.close()
