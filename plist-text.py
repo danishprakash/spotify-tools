d97af3e1d3452c429f1bfb086116e5d1d7a9665b
Raoel Oomen <Raoel.Oomen@topicus.nl>
2023-11-14 12:57:07 +0100

    fix bs4 does not support 'property' anymore or something? this works

--- plist-text.py
+++ plist-text.py
@@ -2,7 +2,7 @@ import bs4, requests, re, sys
 
 playlistReq =requests.get(sys.argv[1])
 playlistSoup = bs4.BeautifulSoup(playlistReq.text, "lxml")
-songUrl = playlistSoup.find_all('meta', property='music:song')
+songUrl = playlistSoup.find_all('meta', attrs={ 'name': 'music:song'})
 
 reg = re.sub(r', a playlist.*', '', playlistSoup.find('meta', property='og:title')['content'])
 
@@ -15,7 +15,7 @@ for i in range(len(songUrl)):
     songSoup = bs4.BeautifulSoup(songReq.text, "lxml")
     songName = songSoup.find('meta', property='og:title')
 
-    artistUrl = songSoup.find('meta', property='music:musician')
+    artistUrl = songSoup.find('meta', attrs={ 'name': 'music:musician'})
     artistReq = requests.get(artistUrl['content'])
     artistSoup = bs4.BeautifulSoup(artistReq.text, "lxml")
     artistName = artistSoup.find('meta', property='og:title')
